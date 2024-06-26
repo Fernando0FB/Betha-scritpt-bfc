62ada98a709ec2da15a3c80dff023cba100cad53
fernando.bonetti <FernandoFBonetti@hotmail.com>
2024-06-26 17:15:43 -0300

    Adicionando função de validação de token

--- Funcoes/httpUtils.groovy
+++ Funcoes/httpUtils.groovy
@@ -62,3 +62,30 @@ def Object confereLote(String idLote, String url, String v_bearerToken){
     }
     return lote;
 }
+
+
+/*
+Função responsável por realizar a validação do token informado.
+ */
+def Map<Boolean, String> validaToken(String token) {
+    //token: Token de migração, ou de tela.
+    String url = ""; //Aqui deve ser inserido a url do tokenInfo
+                    // └> Por motivos de segurança, não foi informado
+    req = Http.servico(url + token).GET();
+
+    if(req.sucesso()) {
+        if(req.json().expired) {
+            return [false:"Token expirado!"];
+        } else {
+            return [true:""]
+        }
+    }
+
+    return [false:"Token não encontrado!"];
+}
+validaToken("Imagine um Token válido");
+//└> retorno -> {true:""}
+validaToken("12312038");
+//└> retorno -> {false:Token não encontrado!}
+validaToken("Imagine um token expirado");
+//└> retorno -> {false:Token expirado!}
